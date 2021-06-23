719a810c8e7420f568fe2fa6ace5c5858b78c41e
Michał Kolanecki <kolanek2@wp.pl>
2021-06-23 21:48:57 +0200

    Add main JavaScript functionality to application

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-const goodAnswers = [c, c, b, b]
+const goodAnswers = ['c', 'c', 'b', 'b'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    let score = 0;
+    const userAnswers = [form.question1.value, form.question2.value, form.question3.value, form.question4.value]
+    
+    // CHECKING ANWERS
+    userAnswers.forEach((answer, index) => {
+        if(answer === goodAnswers[index]){
+            score += 25;
+        }
+    });
+    // SHOW RESULT
+    result.querySelector('span').textContent = `${score} %`;
+    result.classList.remove('d-none');
+});
