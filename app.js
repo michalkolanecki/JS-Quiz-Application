476fb232dc254c2e08324c7dd3155436dccef10c
Michał Kolanecki <kolanek2@wp.pl>
2021-06-24 06:48:00 +0200

    Create function which animated score

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    
     let score = 0;
-    const userAnswers = [form.question1.value, form.question2.value, form.question3.value, form.question4.value]
-    
+    const userAnswers = [form.question1.value, form.question2.value, form.question3.value, form.question4.value];
     // CHECKING ANWERS
     userAnswers.forEach((answer, index) => {
         if(answer === goodAnswers[index]){
@@ -15,6 +13,15 @@ form.addEventListener('submit', e => {
         }
     });
     // SHOW RESULT
-    result.querySelector('span').textContent = `${score} %`;
+    scrollTo(0,0);
     result.classList.remove('d-none');
+    let animation = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${animation} %`;
+        if(animation === score){
+            clearInterval(timer);
+        } else {
+            animation++;
+        }
+    }, 10);
 });
